840c2fcaa4ed88405e03369e5ce13617ad7764c9
ChrisMiuchiz <chris000005@gmail.com>
2020-04-26 15:47:54 -0400

    Add logging and allow for arbitrary length serials

--- server.py
+++ server.py
@@ -1,18 +1,36 @@
 from flask import Flask, request
 from enum import Enum
 from encryption import *
+import platform
+from datetime import datetime
+
+if platform.system() == 'Linux':
+    LOG_PATH = '/var/log/picroma-authentication.log'
+else:
+    LOG_PATH = 'picroma-authentication.log'
 
 Activation = Enum('Activation', 'Logout Activate Login Validate Deactivate')
 SHEET_PLX = 'sheet.plx'
 
 app = Flask(__name__)
 
+def Log(message):
+    now = str(datetime.now())
+    fmtstring = f'[{now}] {message}'
+    print(fmtstring)
+    with open(LOG_PATH, 'a') as f:
+        f.write(fmtstring)
+        f.write('\n')
+    
 
 def AttemptActivation(encryptedSessionID, activationType):
     sessionID = decrypt(unhexdump(encryptedSessionID), PLASMA_CLIENT_KEY)
-    serial, machineID = sessionID[:16].decode(), sessionID[16:]
+    serial, machineID = sessionID[:-160].decode(), sessionID[-160:]
     machineID = decrypt(machineID, PLASMA_SERVER_KEY)
-    print(f'ACTIVATION: {activationType.name}\nSERIAL: {serial}\nMACHINE: {machineID}')
+    ip = request.remote_addr
+    Log(f'{ip}: Activation: {activationType.name}')
+    Log(f'{ip}: Serial: {serial}')
+    Log(f'{ip}: Decrypted machine ID: {machineID}')
     with open(SHEET_PLX, 'rb') as f:
         plx = f.read()
     encrypted_plx = encrypt(plx, machineID)
